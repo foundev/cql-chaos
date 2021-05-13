05c3c2d260dc610b02a3306c15adf64280f72c90
Ryan SVIHLA <tech@foundev.pro>
2021-05-13 11:45:48 +0200

    adding more output

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"math/rand"
 	"strings"
+	"sync"
+	"sync/atomic"
 
 	"github.com/gocql/gocql"
 )
@@ -13,7 +15,7 @@ import (
 func main() {
 	hosts := flag.String("hosts", "localhost", "specify hosts separated by comma")
 	port := flag.Int("port", 9042, "port to use to connect too")
-	threads := flag.Int("threads", 8, "number of threads to use")
+	threads := flag.Int("threads", 100, "number of threads to use")
 	records := flag.Int64("records", 1000000, "total number of records to write")
 	flag.Parse()
 	rawHosts := strings.Split(*hosts, ",")
@@ -28,14 +30,33 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	results := make(chan error, *threads)
+
+	var wg sync.WaitGroup
+
+	var success int64
+	var errors int64
+	threadsFlight := int64(*threads)
+	progressInterval := int64(10000)
 	var i int64
 	for i = 0; i < *records; i++ {
+		if i%progressInterval == 0 {
+			log.Printf("%v of %v records attempted", i+1, *records)
+		}
+		if i%threadsFlight == 0 {
+			wg.Wait()
+		}
+		wg.Add(1)
 		go func() {
-			results <- session.Query(`INSERT INTO test.testers (id, values, counter) VALUES (?, ?, ?)`,
-				rand.Int31(), randomStr, rand.Int31()).Exec()
+			if err = session.Query(`INSERT INTO test.testers (id, values, counter) VALUES (?, ?, ?)`,
+				rand.Int31(), randomStr, rand.Int31()).Exec(); err != nil {
+				log.Printf("ERROR - %v", err)
+				atomic.AddInt64(&errors, 1)
+			} else {
+				atomic.AddInt64(&success, 1)
+			}
 		}()
 	}
+	log.Printf("%v successful %v failed", success, errors)
 }
 
 func randomStr() string {
